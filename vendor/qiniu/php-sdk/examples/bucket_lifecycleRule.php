4259774d16aa95588c8ae2769f87c5ae0d1a9d75
iwei000 <iwei000@outlook.com>
2024-08-09 23:58:54 +0800

    20240809

--- vendor/qiniu/php-sdk/examples/bucket_lifecycleRule.php
+++ vendor/qiniu/php-sdk/examples/bucket_lifecycleRule.php
@@ -2,27 +2,24 @@
 require_once __DIR__ . '/../autoload.php';
 
 use Qiniu\Auth;
+use Qiniu\Config;
+use Qiniu\Storage\BucketManager;
 
 $accessKey = getenv('QINIU_ACCESS_KEY');
 $secretKey = getenv('QINIU_SECRET_KEY');
 
 $auth = new Auth($accessKey, $secretKey);
-$config = new \Qiniu\Config();
-$bucketManager = new \Qiniu\Storage\BucketManager($auth, $config);
-
-$bucket = 'xxxx';
-$name = 'demo';
-$prefix = 'test';
-$delete_after_days = 80;
-$to_line_after_days =70;
+$config = new Config();
+$bucketManager = new BucketManager($auth, $config);
 
 list($Info, $err) = $bucketManager->bucketLifecycleRule(
-    $bucket,
-    $name,
-    $prefix,
-    $delete_after_days,
-    $to_line_after_days
+    $bucket = 'xxxx',
+    $name = 'demo',
+    $prefix = 'test',
+    $delete_after_days = 80,
+    $to_line_after_days = 70
 );
+
 if ($err) {
     print_r($err);
 } else {
