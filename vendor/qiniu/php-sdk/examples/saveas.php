4f7a83395082976e0adcf8ae559fb888d3871058
iwei000 <iwei000@outlook.com>
2024-08-31 16:47:57 +0800

    new URL

--- vendor/qiniu/php-sdk/examples/saveas.php
+++ vendor/qiniu/php-sdk/examples/saveas.php
@@ -4,25 +4,24 @@ require_once __DIR__ . '/../autoload.php';
 use Qiniu\Auth;
 use Qiniu\Processing\PersistentFop;
 
-// 后台来获取AK, SK
+// 获取AK, SK
 $accessKey = 'Access_Key';
 $secretKey = 'Secret_Key';
 
-//生成EncodedEntryURI的值
-$entry = '<bucket>:<Key>';//<Key>为生成缩略图的文件名
-//生成的值
+// 生成EncodedEntryURI的值
+$entry = '<bucket>:<Key>'; // <Key>为生成缩略图的文件名
 $encodedEntryURI = \Qiniu\base64_urlSafeEncode($entry);
 
-//使用SecretKey对新的下载URL进行HMAC1-SHA1签名
+// 构建新的下载URL
 $newurl = "78re52.com1.z0.glb.clouddn.com/resource/Ship.jpg?imageView2/2/w/200/h/200|saveas/" . $encodedEntryURI;
 
+// 使用SecretKey对新的下载URL进行HMAC1-SHA1签名并进行Base64编码
 $sign = hash_hmac("sha1", $newurl, $secretKey, true);
-
-//对签名进行URL安全的Base64编码
 $encodedSign = \Qiniu\base64_urlSafeEncode($sign);
-//最终得到的完整下载URL
+
+// 最终得到的完整下载URL
 $finalURL = "http://" . $newurl . "/sign/" . $accessKey . ":" . $encodedSign;
 
-$callbackBody = file_get_contents("$finalURL");
-
+// 获取并输出回调内容
+$callbackBody = file_get_contents($finalURL);
 echo $callbackBody;
